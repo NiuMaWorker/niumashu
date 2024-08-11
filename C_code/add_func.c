f12358d47eb0f0993f130d608b3f459860d3ff17
OpenTech <960434451@qq.com>
2024-08-11 10:31:39 +0800

    xiaoMa 删除了一些文件。

--- C_code/add_func.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int add_func(int add_a, int add_b)
-{
-    printf("add_a = %d,add_b = %d\n",add_a, add_b);
-    return add_a + add_b;
-}
--- C_code/add_func.h
+++
@@ -1,6 +0,0 @@
-/*
-add_func.h
-*/
-#include <stdio.h>
-
-int add_func(int add_a, int add_b);
